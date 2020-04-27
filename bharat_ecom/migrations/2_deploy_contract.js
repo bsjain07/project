d67be6944bfb174ff93e2cb8b454b9fb5689f503
bsjain07 <bsjain@outlook.com>
2020-04-27 18:04:40 +0530

    Remove duplicated directory

--- bharat_ecom/migrations/2_deploy_contract.js
+++
@@ -1,9 +0,0 @@
-
-var Supplier = artifacts.require("./Supplier.sol");
-var Customer = artifacts.require("./Customer.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Supplier);
-  deployer.deploy(Customer);
- };
-  
